c5f78fe35295688760473834967c20edf9bc47ab
ZB1856 <96122715+ZB1856@users.noreply.github.com>
2021-12-14 13:16:10 +0000

    Update vsb-dc.py

--- vsb-dc.py
+++ vsb-dc.py
@@ -1,5 +1,5 @@
 while True:
-    if input("press 1 for bin to denary ") == "1":
+    if input("press 1 for bin to denary, anything else is denary to binary ") == "1":
         try:
             print(int(input("enter binary number "), 2))
         except ValueError as ex:
