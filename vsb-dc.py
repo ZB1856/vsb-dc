ba89f92f91f0ec21f190fc2c573fb5fcd56848d0
ZB1856 <96122715+ZB1856@users.noreply.github.com>
2021-12-14 13:30:51 +0000

    Update vsb-dc.py

--- vsb-dc.py
+++ vsb-dc.py
@@ -6,6 +6,11 @@ while True:
             print("thats not binary bro")
     else:
         try:
-            print(str(bin(int(input("enter denary number "))))[2:])
+            thing1 = int(input("enter denary number "))
+            eggs = str(bin(int(thing1)))[2:]
+            print(eggs)
+            if thing1 <= 255:
+                print(f"8bit: {('0'*(8-len(eggs)))}{eggs}")
         except ValueError as ex2:
             print("Not a number my dude")
+            
